51730c698fd2d5a807f88efe99bb27c8d57ec4dc
techleonic <techleonic@gmail.com>
2025-05-05 13:43:03 -0600

    Varibles Types

---
+++ Basic/intro.py
@@ -0,0 +1,16 @@
+"""
+Variables and Data Types in Python
+- Variables are used to store data values.
+- Python has no command for declaring a variable.
+- A variable is created the moment you first assign a value to it.
+- Variables do not need to be declared with any particular type and can even change type after they have been set.
+- Variable names can be any length and can consist of letters, numbers, and underscores.
+- Variable names must start with a letter or an underscore (_), followed by letters, numbers, or underscores.
+"""
+
+# Example of variable assignment
+x = 5  # x is of type int
+y = "Hello"  # y is of type str 
+z = 3.14  # z is of type float
+a = True  # a is of type bool
+b = None  # b is of type NoneType
